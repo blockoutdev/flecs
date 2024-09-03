a59a24c4f4c6c8d46bb2c338174c74d3fe507fb9
Sander Mertens <sander.mertens8@gmail.com>
2024-09-03 10:28:32 -0700

    Add "Foo, (ChildOf, $var)" tests

--- test/query/src/Flatten.c
+++ test/query/src/Flatten.c
@@ -255,6 +255,98 @@ void Flatten_fixed_src_childof_any(void) {
     ecs_fini(world);
 }
 
+void Flatten_fixed_src_childof_var(void) {
+    ecs_world_t *world = ecs_mini();
+
+    ecs_entity_t p = ecs_new_w_id(world, EcsPrefab);
+    ecs_entity_t c_1 = ecs_new_w_pair(world, EcsChildOf, p);
+    ecs_entity_t c_2 = ecs_new_w_pair(world, EcsChildOf, p);
+    ecs_entity_t c_3 = ecs_new_w_pair(world, EcsChildOf, p);
+
+    ecs_entity_t i = ecs_new_w_pair(world, EcsIsA, p);
+    const EcsChildren *children = ecs_get_pair(
+        world, i, EcsChildren, EcsChildOf);
+    test_assert(children != NULL);
+
+    ecs_entity_t *child_ids = ecs_vec_first(&children->children);
+    test_assert(child_ids != NULL);
+    test_int(ecs_vec_count(&children->children), 3);
+    test_assert(ecs_has_pair(world, child_ids[0], EcsIsA, c_1));
+    test_assert(ecs_has_pair(world, child_ids[1], EcsIsA, c_2));
+    test_assert(ecs_has_pair(world, child_ids[2], EcsIsA, c_3));
+
+    {
+        ecs_query_t *q = ecs_query(world, {
+            .terms = {
+                { .first.id = EcsChildOf, .second.name = "$x", .src.id = child_ids[0] }
+            },
+            .cache_kind = cache_kind
+        });
+
+        test_assert(q != NULL);
+        int x_var = ecs_query_find_var(q, "x");
+
+        ecs_iter_t it = ecs_query_iter(world, q);
+        test_bool(true, ecs_query_next(&it));
+        test_int(0, it.count);
+        test_uint(child_ids[0], ecs_field_src(&it, 0));
+        test_uint(ecs_pair(EcsChildOf, i), ecs_field_id(&it, 0));
+        test_uint(i, ecs_iter_get_var(&it, x_var));
+        test_bool(true, ecs_field_is_set(&it, 0));
+        test_bool(false, ecs_query_next(&it));
+
+        ecs_query_fini(q);
+    }
+
+    {
+        ecs_query_t *q = ecs_query(world, {
+            .terms = {
+                { .first.id = EcsChildOf, .second.name = "$x", .src.id = child_ids[1] }
+            },
+            .cache_kind = cache_kind
+        });
+
+        test_assert(q != NULL);
+        int x_var = ecs_query_find_var(q, "x");
+
+        ecs_iter_t it = ecs_query_iter(world, q);
+        test_bool(true, ecs_query_next(&it));
+        test_int(0, it.count);
+        test_uint(child_ids[1], ecs_field_src(&it, 0));
+        test_uint(ecs_pair(EcsChildOf, i), ecs_field_id(&it, 0));
+        test_uint(i, ecs_iter_get_var(&it, x_var));
+        test_bool(true, ecs_field_is_set(&it, 0));
+        test_bool(false, ecs_query_next(&it));
+
+        ecs_query_fini(q);
+    }
+
+    {
+        ecs_query_t *q = ecs_query(world, {
+            .terms = {
+                { .first.id = EcsChildOf, .second.name = "$x", .src.id = child_ids[2] }
+            },
+            .cache_kind = cache_kind
+        });
+
+        test_assert(q != NULL);
+        int x_var = ecs_query_find_var(q, "x");
+
+        ecs_iter_t it = ecs_query_iter(world, q);
+        test_bool(true, ecs_query_next(&it));
+        test_int(0, it.count);
+        test_uint(child_ids[2], ecs_field_src(&it, 0));
+        test_uint(ecs_pair(EcsChildOf, i), ecs_field_id(&it, 0));
+        test_uint(i, ecs_iter_get_var(&it, x_var));
+        test_bool(true, ecs_field_is_set(&it, 0));
+        test_bool(false, ecs_query_next(&it));
+
+        ecs_query_fini(q);
+    }
+
+    ecs_fini(world);
+}
+
 void Flatten_this_childof_parent(void) {
     ecs_world_t *world = ecs_mini();
 
@@ -2935,6 +3027,105 @@ void Flatten_this_written_childof_any(void) {
     test_uint(ecs_pair(EcsChildOf, EcsWildcard), ecs_field_id(&it, 1));
     test_bool(true, ecs_field_is_set(&it, 0));
     test_bool(true, ecs_field_is_set(&it, 1));
+
+    test_bool(false, ecs_query_next(&it));
+
+    ecs_query_fini(q);
+
+    ecs_fini(world);
+}
+
+void Flatten_this_written_childof_var(void) {
+    ecs_world_t *world = ecs_mini();
+
+    ECS_TAG(world, Foo);
+
+    ecs_entity_t p = ecs_new_w_id(world, EcsPrefab);
+    ecs_entity_t c_1 = ecs_new_w_pair(world, EcsChildOf, p);
+    ecs_entity_t c_2 = ecs_new_w_pair(world, EcsChildOf, p);
+    ecs_entity_t c_3 = ecs_new_w_pair(world, EcsChildOf, p);
+
+    ecs_add(world, c_1, Foo);
+    ecs_add(world, c_2, Foo);
+    ecs_add(world, c_3, Foo);
+
+    ecs_entity_t i = ecs_new_w_pair(world, EcsIsA, p);
+    const EcsChildren *children = ecs_get_pair(
+        world, i, EcsChildren, EcsChildOf);
+    test_assert(children != NULL);
+
+    ecs_entity_t *child_ids = ecs_vec_first(&children->children);
+    test_assert(child_ids != NULL);
+    test_int(ecs_vec_count(&children->children), 3);
+    test_assert(ecs_has_pair(world, child_ids[0], EcsIsA, c_1));
+    test_assert(ecs_has_pair(world, child_ids[1], EcsIsA, c_2));
+    test_assert(ecs_has_pair(world, child_ids[2], EcsIsA, c_3));
+
+    ecs_entity_t ic_1 = ecs_new_w_pair(world, EcsChildOf, i);
+    ecs_entity_t ic_2 = ecs_new_w_pair(world, EcsChildOf, i);
+    ecs_entity_t ic_3 = ecs_new_w_pair(world, EcsChildOf, i);
+
+    ecs_add(world, ic_1, Foo);
+    ecs_add(world, ic_2, Foo);
+    ecs_add(world, ic_3, Foo);
+
+    ecs_new_w(world, Foo); /* Dummy entity */
+
+    ecs_query_t *q = ecs_query(world, {
+        .terms = {
+            { Foo }, 
+            { .first.id = EcsChildOf, .second.name = "$x" }
+        },
+        .cache_kind = cache_kind
+    });
+
+    test_assert(q != NULL);
+    int x_var = ecs_query_find_var(q, "x");
+    test_assert(x_var != -1);
+
+    ecs_iter_t it = ecs_query_iter(world, q);
+    test_bool(true, ecs_query_next(&it));
+    test_int(1, it.count);
+    test_uint(it.entities[0], child_ids[0]);
+    test_assert(it.table == ecs_get_table(world, child_ids[0]));
+    test_uint(Foo, ecs_field_id(&it, 0));
+    test_uint(ecs_pair(EcsChildOf, i), ecs_field_id(&it, 1));
+    test_uint(i, ecs_iter_get_var(&it, x_var));
+    test_bool(true, ecs_field_is_set(&it, 0));
+    test_bool(true, ecs_field_is_set(&it, 1));
+
+    test_bool(true, ecs_query_next(&it));
+    test_int(1, it.count);
+    test_uint(it.entities[0], child_ids[1]);
+    test_assert(it.table == ecs_get_table(world, child_ids[1]));
+    test_uint(Foo, ecs_field_id(&it, 0));
+    test_uint(ecs_pair(EcsChildOf, i), ecs_field_id(&it, 1));
+    test_uint(i, ecs_iter_get_var(&it, x_var));
+    test_bool(true, ecs_field_is_set(&it, 0));
+    test_bool(true, ecs_field_is_set(&it, 1));
+
+    test_bool(true, ecs_query_next(&it));
+    test_int(1, it.count);
+    test_uint(it.entities[0], child_ids[2]);
+    test_assert(it.table == ecs_get_table(world, child_ids[2]));
+    test_uint(Foo, ecs_field_id(&it, 0));
+    test_uint(ecs_pair(EcsChildOf, i), ecs_field_id(&it, 1));
+    test_uint(i, ecs_iter_get_var(&it, x_var));
+    test_bool(true, ecs_field_is_set(&it, 0));
+    test_bool(true, ecs_field_is_set(&it, 1));
+
+    test_bool(true, ecs_query_next(&it));
+    test_int(3, it.count);
+    test_uint(it.entities[0], ic_1);
+    test_uint(it.entities[1], ic_2);
+    test_uint(it.entities[2], ic_3);
+    test_assert(it.table == ecs_get_table(world, ic_1));
+    test_assert(it.table == ecs_get_table(world, ic_2));
+    test_assert(it.table == ecs_get_table(world, ic_3));
+    test_uint(Foo, ecs_field_id(&it, 0));
+    test_uint(ecs_pair(EcsChildOf, i), ecs_field_id(&it, 1));
+    test_uint(i, ecs_iter_get_var(&it, x_var));
+    test_bool(true, ecs_field_is_set(&it, 0));
 
     test_bool(false, ecs_query_next(&it));
 
--- test/query/src/main.c
+++ test/query/src/main.c
@@ -1906,6 +1906,7 @@ void Flatten_setup(void);
 void Flatten_fixed_src_childof_parent(void);
 void Flatten_fixed_src_childof_wildcard(void);
 void Flatten_fixed_src_childof_any(void);
+void Flatten_fixed_src_childof_var(void);
 void Flatten_this_childof_parent(void);
 void Flatten_this_childof_parent_no_children(void);
 void Flatten_this_childof_parent_only_regular(void);
@@ -1943,6 +1944,7 @@ void Flatten_this_written_childof_parent_w_component_inherited(void);
 void Flatten_this_written_childof_wildcard(void);
 void Flatten_this_written_childof_wildcard_mixed(void);
 void Flatten_this_written_childof_any(void);
+void Flatten_this_written_childof_var(void);
 void Flatten_var_written_childof_parent(void);
 void Flatten_var_written_childof_parent_w_component(void);
 void Flatten_var_written_childof_parent_w_component_inherit(void);
@@ -9618,6 +9620,10 @@ bake_test_case Flatten_testcases[] = {
         Flatten_fixed_src_childof_any
     },
     {
+        "fixed_src_childof_var",
+        Flatten_fixed_src_childof_var
+    },
+    {
         "this_childof_parent",
         Flatten_this_childof_parent
     },
@@ -9764,6 +9770,10 @@ bake_test_case Flatten_testcases[] = {
     {
         "this_written_childof_any",
         Flatten_this_written_childof_any
+    },
+    {
+        "this_written_childof_var",
+        Flatten_this_written_childof_var
     },
     {
         "var_written_childof_parent",
@@ -10841,7 +10851,7 @@ static bake_test_suite suites[] = {
         "Flatten",
         Flatten_setup,
         NULL,
-        43,
+        45,
         Flatten_testcases,
         1,
         Flatten_params
